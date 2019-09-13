3332abcc28b565ca6a555f7d18ef85ffc89e0bcb
mengyuan <myyan92@gmail.com>
2019-09-13 15:06:19 -0700

    scaling up data collection

--- data_collection.py
+++ data_collection.py
@@ -1,31 +1,39 @@
 import numpy as np
 import random
+import json
+import os
 from advanced_buffer import Buffer, get_reward_key
 from knot_env import KnotEnv
 import pdb
 
 def hash_dict(abstract_action):
-    tokens = [k+':'+str(v) for k,v in abstract_action.items()]
-    return ' '.join(sorted(tokens))
+    tokens = [k+'-'+str(v) for k,v in abstract_action.items()]
+    return '_'.join(sorted(tokens))
 
 env = KnotEnv(60)
-buffer = Buffer('move-cross_endpoint-over_sign-1', size=1000)
+env.reset()
+with open('data_collection_heuristic.json', 'r') as f:
+    annotations = json.load(f)
 
-while buffer.num_in_buffer < 100:
-    obs = env.reset()
-    intended_action = {'move':'cross', 'over_idx':2, 'under_idx':0, 'sign':1}
-    action_gaussian_mean = np.array([0.95, 0.18, 0.1, 0.15, -0.1, 0.05])
-    action_gaussian_std = np.array([0.03, 0.05, 0.05, 0.05, 0.05, 0.03])
-#    intended_action = {'move':'cross', 'over_idx':2, 'under_idx':1, 'sign':1}
-#    action_gaussian_mean = np.array([0.95, 0.30, 0.1, 0.32, -0.1, 0.05])
-#    action_gaussian_std = np.array([0.03, 0.05, 0.05, 0.05, 0.05, 0.03])
-    actions = np.random.normal(loc=action_gaussian_mean, scale=action_gaussian_std, size=(env.parallel, 6))
-    actions = np.clip(actions, env.action_low, env.action_high)
-    _, rewards, dones, infos = env.step(actions)
-    for ob,ac,r in zip(obs, actions, rewards):
-        if hash_dict(r) == hash_dict(intended_action):
-            buffer.put(ob, ac, r)
-    print(buffer.num_in_buffer)
-    #env.render()
+for annotation_item in annotations[14:16]:
+    action_name = hash_dict(annotation_item['intended_action'])
+    action_gaussian_mean = np.array(annotation_item['action_gaussian_mean'])
+    action_gaussian_std = np.array(annotation_item['action_gaussian_std'])
 
-buffer.dump()
+    print("collecting ", action_name)
+    reward_key = get_reward_key(annotation_item['intended_action'], env.start_state[0])
+    buffer = Buffer(reward_key, size=1000)
+
+    while buffer.num_in_buffer < 100:
+        obs = env.reset()
+        actions = np.random.normal(loc=action_gaussian_mean, scale=action_gaussian_std, size=(env.parallel, 6))
+        actions = np.clip(actions, env.action_low, env.action_high)
+        _, rewards, dones, infos = env.step(actions)
+        for ob,ac,r in zip(obs, actions, rewards):
+            if hash_dict(r) == action_name:
+                buffer.put(ob, ac, r)
+        print(buffer.num_in_buffer)
+        env.render()
+
+    buffer.dump()
+    os.rename(reward_key+'_buffer.npz', action_name+'_init_buffer.npz')
