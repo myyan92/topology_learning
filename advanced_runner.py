aae19cd0a1a244ef4fa6af179a44d68761d8d7f9
mengyuan <myyan92@gmail.com>
2019-10-19 23:11:14 -0700

    extend to planning topo action for each obs

--- advanced_runner.py
+++ advanced_runner.py
@@ -2,16 +2,17 @@ import numpy as np
 import random
 from advanced_buffer import get_reward_key
 from model_stats import ModelStats
-from planner import get_intended_action, encode
+from planner import get_random_action, get_fixed_action, encode
 from topology.state_2_topology import state2topology
 from state_encoder import unifying_transform_encode, unifying_transform_decode
+import gin
 import pdb
 
 def hash_dict(abstract_action):
     tokens = [k+':'+str(v) for k,v in abstract_action.items()]
     return ' '.join(sorted(tokens))
 
-
+@gin.configurable
 class Runner(object):
     """
     We use this class to generate batches of experiences
@@ -22,41 +23,68 @@ class Runner(object):
     run():
     - Make a mini batch of experiences
     """
-    def __init__(self, env, models, model_stats, buffers, gamma=0.99):
+    def __init__(self, env, models, model_stats, buffers,
+                 topo_action_func=get_random_action, explore=True,
+                 eval_save=False, eval_render=False,  gamma=0.99):
         self.env = env
         self.model_dict = {model.scope:model for model in models}
         self.model_stats_dict = {model_stat.model_name:model_stat for model_stat in model_stats}
         self.buffer_dict = {buffer.reward_key:buffer for buffer in buffers}
         self.obs = env.reset()
+        self.topo_action_func = topo_action_func
+        self.explore = explore
+        self.eval_save = eval_save
+        self.eval_render = eval_render
+
+        if self.eval_save:
+            self.count = 0
 
         self.gamma = gamma # TODO for RRT env?
 
     def run(self, sess):
-        intended_action = get_intended_action(self.obs[0]) # assumes all the same for each batch
-        trans_obs = []
-        for obs in self.obs:
-            obs_u, _, trans_intended_action, transform = unifying_transform_encode(obs, None, intended_action)
+        intended_action = [self.topo_action_func(ob, self.model_dict.keys()) for ob in self.obs]
+        trans_obs, trans_intended_actions, transforms = [], [], []
+        for obs, ia in zip(self.obs, intended_actions):
+            obs_u, _, ia_u, transform = unifying_transform_encode(obs, None, ia)
             trans_obs.append(obs_u)
-        model = self.model_dict[get_reward_key(trans_intended_action, trans_obs[0])] # hard coded
-        model_inputs = encode(trans_obs, [trans_intended_action]*len(self.obs))
-        trans_actions = model.predict_batch(sess, *model_inputs, explore=True)
-        trans_actions = np.clip(trans_actions, self.env.action_low, self.env.action_high)
+            trans_intended_actions.append(ia_u)
+            transforms.append(transform)
+        reward_keys = [get_reward_key(ia_u, obs_u) for ia_u, obs_u in zip(trans_intended_actions, trans_obs)]
+        trans_actions = [None]*len(self.obs)
+        model_keys = set(reward_keys)
+        for key in model_keys:
+            model = self.model_dict[key]
+            sublist_trans_obs = [ob_u for ob_u, k in zip(trans_obs, reward_keys) if k==key]
+            sublist_trans_ia = [ia_u for ia_u, k in zip(trans_intended_actions, reward_keys) if k==key]
+            model_inputs = encode(sublist_trans_obs, sublist_trans_ia)
+            sublist_trans_actions = model.predict_batch(sess, *model_inputs, explore=self.explore)
+            sublist_trans_actions = np.clip(sublist_trans_actions, self.env.action_low, self.env.action_high)
+            idx = 0
+            for i,k in enumerate(reward_keys):
+                if k==key:
+                    trans_actions[i]=sublist_trans_actions[idx]
+                    idx += 1
+
         actions = []
-        for tac in trans_actions:
-            _, ac, _ = unifying_transform_decode(None, tac, None, transform)
+        for tac, tf in zip(trans_actions, transforms):
+            _, ac, _ = unifying_transform_decode(None, tac, None, tf)
             actions.append(ac)
         actions = np.array(actions)
 
         obs, rewards, dones, infos = self.env.step(actions)
-        batch_r = []
-        for ob,ac,r in zip(trans_obs, trans_actions, rewards):
-            stats = self.model_stats_dict[model.scope]
-            reward = 1.0 if hash_dict(r) == hash_dict(intended_action) else 0.0
+
+        for ob_u, ac_u, r, ia, ia_u, key in zip(trans_obs, trans_actions, rewards, intended_actions, trans_intended_actions, reward_keys):
+            stats = self.model_stats_dict[key]
+            reward = 1.0 if hash_dict(r) == hash_dict(ia) else 0.0
             stats.put(reward)
-            reward_key = get_reward_key(trans_intended_action, ob)
-            if reward_key in self.buffer_dict:
-                self.buffer_dict[reward_key].put(ob, ac, reward, trans_intended_action)
-            batch_r.append(reward)
-        print(hash_dict(intended_action), np.mean(batch_r))
+            if key in self.buffer_dict:
+                self.buffer_dict[key].put(ob_u, ac_u, reward, ia_u)
+
+        if self.eval_render:
+            self.env.render()
+        if self.eval_save:
+            for ob,ia,r in zip(obs, intended_actions, rewards):
+                if hash_dict(r) == hash_dict(ia):
+                    np.savetxt('end_state_%d.txt'%(self.count), ob)
+                    self.count += 1
         self.obs = self.env.reset()
-        # TODO how to coordinate multiple policies?
