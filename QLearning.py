93006b43ae6bfe4a4d7a80b2638d482389aa8742
Ubuntu <ubuntu@ip-172-31-1-5.us-west-1.compute.internal>
2020-02-12 20:12:05 +0000

    fix runner and add actor-critic to QLearning.

--- QLearning.py
+++ QLearning.py
@@ -1,5 +1,5 @@
 import time
-import os, sys
+import os, sys, glob, shutil
 import signal
 import pdb
 import tensorflow as tf
@@ -10,21 +10,23 @@ import numpy as np
 from knot_env import KnotEnv
 from advanced_runner import Runner
 from advanced_buffer import Buffer
-from model_GRU_C3 import Model as Model_v3
-from model_GRU_attention_C5 import Model as Model_v5
+from model_GRU_C3 import Model as Model_Critic_v3
+from model_GRU_attention_3 import Model as Model_Actor_v3
 from model_stats import ModelStats
-import matplotlib.pyplot as plt
 import gin
 
 
 class A2C():
-    def __init__(self, models, model_stats, buffers, log_interval,
+    def __init__(self, actor_models, critic_models, model_stats, buffers, log_interval,
                  train_batch_size, replay_start, replay_grow, save_dir):
-        self.model_dict = {model.scope:model for model in models}
+        self.actor_model_dict = {model.scope.replace('_actor',''):model for model in actor_models}
+        self.critic_model_dict = {model.scope.replace('_critic',''):model for model in critic_models}
         self.model_stat_dict = {model_stat.model_name:model_stat for model_stat in model_stats}
-        self.steps_dict = {model.scope:0 for model in models}
+        self.steps_dict = {model.scope.replace('_critic',''):0 for model in critic_models}
         self.buffer_dict = {buffer.reward_key:buffer for buffer in buffers}
-        assert set(self.model_dict.keys()) == set(self.buffer_dict.keys())
+        assert set(self.critic_model_dict.keys()) == set(self.buffer_dict.keys())
+        if len(actor_models)>0:
+            assert set(self.actor_model_dict.keys()) == set(self.buffer_dict.keys())
 
         self.log_interval = log_interval
         self.train_batch_size = train_batch_size
@@ -36,8 +38,10 @@ class A2C():
         tf_config.gpu_options.allow_growth=True
         self.sess = tf.Session(config=tf_config)
         self.save_dir = save_dir
-        if not os.path.exists(os.path.join(self.save_dir, 'models/')):
-            os.makedirs(os.path.join(self.save_dir, 'models/'))
+        if not os.path.exists(os.path.join(self.save_dir, 'actor_models/')):
+            os.makedirs(os.path.join(self.save_dir, 'actor_models/'))
+        if not os.path.exists(os.path.join(self.save_dir, 'critic_models/')):
+            os.makedirs(os.path.join(self.save_dir, 'critic_models/'))
         self.train_writer = tf.summary.FileWriter(os.path.join(self.save_dir, 'tfboard'), self.sess.graph)
         self.sess.run(tf.global_variables_initializer())
 
@@ -49,11 +53,20 @@ class A2C():
                 # add augmentation
                 obs, actions, over_seg_dict, under_seg_dict, _ = self.buffer_dict[key].augment(
                                                                    obs, actions, over_seg_dict, under_seg_dict)
+                # Train actor with buffer
+                if key in self.actor_model_dict:
+                    self.actor_model_dict[key].fit(self.sess, obs, over_seg_dict, under_seg_dict, actions, rewards, rewards)
+                # Train Q function
                 # make up random bad samples.
                 fake_actions = np.random.uniform(low=np.array([0.0,-0.5,-0.5,-0.5,-0.5,0.02]),
                                                  high=np.array([1.0,0.5,0.5,0.5,0.5,0.2]), size=(self.train_batch_size//2, 6))
                 # negative mining
-                mining_actions = self.model_dict[key].predict_batch_action(self.sess, obs, over_seg_dict, under_seg_dict)
+#                init_action_mean = np.array([0.5, 0.0, 0.0, 0.0, 0.0, 0.1])
+#                init_action_cov = np.diag(np.array([0.3,0.3,0.3,0.3,0.3,0.05])**2)
+#                sample_actions = np.random.multivariate_normal(init_action_mean, init_action_cov, size=(self.train_batch_size//2, 256))
+#                mining_actions = self.critic_model_dict[key].predict_batch_action(self.sess, obs, over_seg_dict, under_seg_dict,
+#                                                                                  init_action_samples = sample_actions,
+#                                                                                  iterations = 5, q_threshold=None)
                 # mixing
 #                fake_actions[0::2] = mining_actions[0::2]
                 obs = np.concatenate([obs,obs], axis=0)
@@ -61,11 +74,13 @@ class A2C():
                 over_seg_dict = {key:np.concatenate([val, val], axis=0) for key,val in over_seg_dict.items()}
                 under_seg_dict = {key:np.concatenate([val, val], axis=0) for key,val in under_seg_dict.items()}
                 rewards = np.concatenate([rewards, np.zeros(len(fake_actions))], axis=0)
-                q_loss = self.model_dict[key].fit_q(self.sess, obs, over_seg_dict, under_seg_dict, actions, rewards[:,np.newaxis])
-                print(q_loss)
+                q_loss = self.critic_model_dict[key].fit_q(self.sess, obs, over_seg_dict, under_seg_dict, actions, rewards[:,np.newaxis])
+
                 self.steps_dict[key] += 1
                 if (self.steps_dict[key] % self.log_interval == 0):
-                    self.model_dict[key].save(self.sess, os.path.join(self.save_dir, 'models', 'model-%s'%(key)) , step=self.steps_dict[key])
+                    if key in self.actor_model_dict:
+                        self.actor_model_dict[key].save(self.sess, os.path.join(self.save_dir, 'actor_models', 'model-%s'%(key)) , step=self.steps_dict[key])
+                    self.critic_model_dict[key].save(self.sess, os.path.join(self.save_dir, 'critic_models', 'model-%s'%(key)) , step=self.steps_dict[key])
                     stat_string = self.model_stat_dict[key].stat()
                     print(stat_string)
 
@@ -73,21 +88,33 @@ class A2C():
 def learn(
     env,
     reward_keys,
-    model_type,
+    actor_model_type,
+    critic_model_type,
     pretrain_buffers,
     total_timesteps=int(80e6),
     train_batch_size=128,
     gamma=0.99,
     lr=1e-4,
+    max_grad_norm=2000.0,
     log_interval=50,
     save_dir='./test'):
 
-    if model_type=='Model_v3':
-        models = [Model_v3(key) for key in reward_keys]
-    if model_type=='Model_v5':
-        models = [Model_v5(key) for key in reward_keys]
+    if actor_model_type=='Model_Actor_v3':
+        actor_models = [Model_Actor_v3(key+'_actor') for key in reward_keys]
+    elif actor_model_type=='None':
+        actor_models = []
+    else:
+        raise ValueError('actor model type not supported')
 
-    for model in models:
+    if critic_model_type=='Model_Critic_v3':
+        critic_models = [Model_Critic_v3(key+'_critic') for key in reward_keys]
+    else:
+        raise ValueError('critic model type not supported')
+
+    for model in actor_models:
+        model.build()
+        model.setup_optimizer(learning_rate=lr, ent_coef=0.0, vf_coef=0.0, max_grad_norm=max_grad_norm)
+    for model in critic_models:
         model.build()
         model.setup_optimizer(learning_rate=lr)
 
@@ -98,12 +125,28 @@ def learn(
     model_stats = [ModelStats(model_name=key) for key in reward_keys]
 
     # pretrain
-    a2c = A2C(models, model_stats, buffers, log_interval, train_batch_size, replay_start=32, replay_grow=0.2, save_dir=save_dir)
-#    for model in models:
-#        model.load(a2c.sess, './1to2-cross-endpointover-sign1-randstate_mC3_debug/models/model-move-cross_endpoint-over_sign-1-2500')
-#        a2c.steps_dict[model.scope]=2500
+    a2c = A2C(actor_models, critic_models, model_stats, buffers, log_interval, train_batch_size, replay_start=32, replay_grow=0.2, save_dir=save_dir)
+#    for model in actor_models:
+#        model.load(a2c.sess, './1to2-cross-endpointover-sign1-randstate_mC3_nomining_AC/actor_models/model-move-cross_endpoint-over_sign-1-102200')
+#        a2c.steps_dict[model.scope.replace('_actor','')]=102200
+#    for model in critic_models:
+#        model.load(a2c.sess, './1to2-cross-endpointover-sign1-randstate_mC3_nomining_AC/critic_models/model-move-cross_endpoint-over_sign-1-102200')
     a2c.update()
-    runner = Runner(env, models, model_stats, buffers, gamma=gamma)
+    # save pretrained models
+    for key, model in a2c.actor_model_dict.items():
+        latest_checkpoint = model.saver.last_checkpoints[-1]
+        files=glob.glob(latest_checkpoint+'*')
+        for f in files:
+            suffix = f.split('.')[-1]
+            shutil.copy(f, os.path.join(a2c.save_dir, 'actor_models', 'model-%s-pretrain.%s'%(key, suffix)))
+    for key, model in a2c.critic_model_dict.items():
+        latest_checkpoint = model.saver.last_checkpoints[-1]
+        files=glob.glob(latest_checkpoint+'*')
+        for f in files:
+            suffix = f.split('.')[-1]
+            shutil.copy(f, os.path.join(a2c.save_dir, 'critic_models', 'model-%s-pretrain.%s'%(key, suffix)))
+
+    runner = Runner(env, actor_models, critic_models, model_stats, buffers, gamma=gamma)
 
     def signal_handler(sig, frame):
         for buffer in buffers:
@@ -125,9 +168,10 @@ def learn(
 
 if __name__ == "__main__":
 
-    model_type = sys.argv[1]
-    gin_config_file = sys.argv[2]
+    actor_model_type = sys.argv[1]
+    critic_model_type = sys.argv[2]
+    gin_config_file = sys.argv[3]
     gin.parse_config_file(gin_config_file)
     env = KnotEnv(parallel=32)
-    learn(env, model_type=model_type)
+    learn(env, actor_model_type=actor_model_type, critic_model_type=critic_model_type)
 
